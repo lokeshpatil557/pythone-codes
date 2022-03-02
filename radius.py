52e7810002f77efa25a14d11f55a076beb51421b
lokeshpatil557 <lokeshpatil557@gmail.com>
2022-03-02 20:38:02 +0530

    my two

---
+++ radius.py
@@ -0,0 +1,38 @@
+"""
+from math import pi
+r=float(input("enter the radius of the circle:"))
+print("area of circle is:"+str(pi*r**2))
+a=float(input("enter the radius of circle:"))
+r=pi*a**2
+print(r)
+
+
+f=float(input("enter the degree in fahrant:"));
+c=(f-32)*5/9
+print(c)
+
+
+a=int(input("enter the number"))
+b=int(input("enter the second number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a**b
+print(a,b,c,d,e,f,g)
+"""
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=int(input("enter the third number"))
+d=a*b*c/2
+print(d,round(1))
+
+
+
+
+
+
+
+
+
+
